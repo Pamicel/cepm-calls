d49c03b8ce8634890b0262336bccd6977fd8bd13
Paul AMICEL <paul.amicel@owkin.com>
2019-11-23 22:18:09 +0100

    fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.post('/twiml/expressions', (req, res) => {
     res.set('Content-Type', 'text/xml');
     res.sendFile('./responses/expressions.xml', { root: '.' });
 });
-app.get('/sound/violin', (req, res) => {
+app.get('/sounds/violin', (req, res) => {
     res.set('Content-Type', 'audio/mpeg');
     res.sendFile('./responses/violin.mp3', { root: '.' });
 });

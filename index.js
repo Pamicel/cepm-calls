7153b38940ddb5f773435afa78a614bb3397d9cc
Paul AMICEL <paul.amicel@owkin.com>
2019-11-23 22:07:18 +0100

    added violin

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const app = express();
 const port = process.env.NODE_ENV === 'development' ? '1234' : process.env.PORT;
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
 app.post('/twiml/expressions', (req, res) => {
     res.set('Content-Type', 'text/xml');
     res.sendFile('./responses/expressions.xml', { root: '.' });
+});
+app.post('/sound/violin', (req, res) => {
+    res.set('Content-Type', 'audio/mpeg');
+    res.sendFile('./responses/violin.mp3', { root: '.' });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
